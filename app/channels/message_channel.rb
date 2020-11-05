8653c418061a61d10f63358b0959ab965f5a2fa4
David T. Allen <148455+bitlather@users.noreply.github.com>
2020-11-05 11:41:36 -0500

    Add some debug info

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,6 +1,13 @@
 class MessageChannel < ApplicationCable::Channel
   def subscribed
-    puts "Subcsribed to MessageChannel; streaming from #{channel_name}"
+    puts
+    puts "------------------------------------------"
+    puts
+    puts "Subscribed to MessageChannel; streaming from #{channel_name}"
+    puts "params: #{params}"
+    puts
+    puts "------------------------------------------"
+    puts
     stream_from channel_name
     MessageJob.perform_later(channel_name, controller, action, params, *identifiers.flatten)
   end
@@ -37,6 +44,6 @@ class MessageChannel < ApplicationCable::Channel
   end
 
   def channel_name
-    "messages:#{route}_#{params[:action]}"
+    "messages:#{route}:#{params[:action]}:#{params[:user_id]}" #*DTA Could add a random value too if we desire
   end
 end
